f61f26996a36b37b50ec1bebc84c149d0664d78a
matheussaldanha22 <matheus.amaral@sptech.school>
2024-11-11 22:39:53 -0300

    atualização style cadastro

--- scripts/scriptCadastro.js
+++ scripts/scriptCadastro.js
@@ -1,6 +1,12 @@
+var checado = false
 function checar(){
     var nome = ipt_nome.value
     var email = ipt_email.value
     var senha = ipt_senha.value
     var confirmSenha = ipt_confirmSenha.value
+
+    var emailValido = email.includes('@') && email.includes('.') 
+    if(!emailValido){
+        alert('Email invalido')
+    }
 }
